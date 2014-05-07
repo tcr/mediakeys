45ea8d29092c63f202c967a21dceb3c2ce475505
Tim Cameron Ryan <id@timryan.org>
2014-05-07 17:41:57 -0400

    And

--- MMShellHook/build.cmd
+++ MMShellHook/build.cmd
@@ -1,1 +1,1 @@
-..\..\gyp\gyp --depth=. binding.gyp -f msvs ; c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBUILD binding.sln ; .\Default\keylistener.exe
+..\..\gyp\gyp --depth=. binding.gyp -f msvs & c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBUILD binding.sln & .\Default\keylistener.exe
