11be8eb20b8aa41e4880c5252692101a34c28f46
Tim Cameron Ryan <id@timryan.org>
2014-05-08 11:45:07 -0400

    linux

--- linux.js
+++ linux.js
@@ -6,8 +6,9 @@ var bus = dbus.getBus('session');
 
 bus.getInterface('org.gnome.SettingsDaemon', '/org/gnome/SettingsDaemon/MediaKeys', 'org.gnome.SettingsDaemon.MediaKeys', function(err, iface) {
 
-	iface.on('MediaPlayerKeyPressed', function(keys) {
-		console.log(keys);
-	});
+iface.addMethod('MediaPlayerKeyPressed', { in: DBus.Define(Array, 'list') }, function(list, callback) {
+	console.log(list);
+	callback(true);
+});
 
 });
