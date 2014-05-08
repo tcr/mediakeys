427782e53b5292aeeb6754c620e7deb8491b7e02
Tim Cameron Ryan <id@timryan.org>
2014-05-08 00:30:09 -0400

    cleanup

--- win/build.cmd
+++
@@ -1,1 +0,0 @@
-..\..\gyp\gyp --depth=. shell.gyp -f msvs & c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBUILD shell.sln
--- win/logga.py
+++
@@ -1,8 +0,0 @@
-from subprocess import Popen, PIPE
-
-p = Popen("Default\\keylistener.exe", stdout=PIPE, bufsize=1)
-for line in iter(p.stdout.readline, ''):
-    print line,
-p.stdout.close()
-if p.wait() != 0:
-   raise RuntimeError("%r failed, exit status: %d" % (cmd, p.returncode))
