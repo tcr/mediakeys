60fa1a521b8fd0cb3931d064cab901a692787dd7
Tim Cameron Ryan <id@timryan.org>
2014-05-08 00:56:33 -0400

    Test

---
+++ linux.js
@@ -0,0 +1,13 @@
+var DBus = require('../');
+
+var dbus = new DBus();
+
+var bus = dbus.getBus('session');
+
+bus.getInterface('org.gnome.SettingsDaemon', '/org/gnome/SettingsDaemon/MediaKeys', 'org.gnome.SettingsDaemon.MediaKeys', function(err, iface) {
+
+	iface.on('MediaPlayerKeyPressed', function(keys) {
+		console.log(keys);
+	});
+
+});
