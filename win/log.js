e12f661f709f7d5ffc07860795716d21eae7d3ca
Tim Cameron Ryan <id@timryan.org>
2014-05-08 00:14:27 -0400

    grace

--- win/log.js
+++ win/log.js
@@ -8,3 +8,15 @@ proc.on('exit', function (code) {
 	console.error('exited with code', code);
 	process.exit(code);
 })
+
+// Only fire once to avoid potential overlap (not that this will happen in a 
+// well-ordered environment, but you never know).
+process.once("SIGTERM", function () {
+ 
+  // Cleanup activities go here...
+  // proc.kill();
+  proc.disconnect();
+ 
+  // Then shutdown.
+  process.exit(0);
+});
