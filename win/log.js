13af4a1a437e9fb76adf7a45490b59974e132c97
Tim Cameron Ryan <id@timryan.org>
2014-05-07 23:58:18 -0400

    node

---
+++ win/log.js
@@ -0,0 +1,10 @@
+var path = require('path');
+var spawn = require('child_process').spawn;
+
+var proc = spawn(path.resolve('Default/keylistener.exe'), []);
+proc.stdout.pipe(process.stdout);
+proc.stderr.pipe(process.stderr);
+proc.on('exit', function (code) {
+	console.error('exited with code', code);
+	process.exit(code);
+})
